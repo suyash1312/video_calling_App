572b5097ea31208fa364523da2328efd7cd1163c
suyash1312 <1999suyashsharma@gmail.com>
2023-05-09 13:00:00 +0530

    This is Video Calling App.

---
+++ lib/main.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+const String appId = "37a5de0ec2694cd089a1cc597b9adba2";
+
+void main() => runApp(const MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String channelName = "SuyashAgora";
+  String token = "007eJxTYLCfvH3ZybesP3dopcRZun3qmN9d+Kk6IqZ/TVrdhex7HU8VGIzNE01TUg1Sk43MLE2SUwwsLBMNk5NNLc2TLBNTkhKNWl9EpjQEMjL0pM5nYWSAQBCfmyG4tDKxOMMxPb8okYEBACHQJUo=";
+
+  int uid = 0; // uid of the local user
+
+  int? _remoteUid; // uid of the remote user
+  bool _isJoined = false; // Indicates if the local user has joined the channel
+  late RtcEngine agoraEngine; // Agora engine instance
+
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey
+  = GlobalKey<ScaffoldMessengerState>(); // Global key to access the scaffold
+  // Build UI
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scaffoldMessengerKey: scaffoldMessengerKey,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+
+            title: const Text('Welcome to video calling APP'),
+          ),
+          body: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            children: [
+              // Container for the local video
+              Container(
+                height: 240,
+                decoration: BoxDecoration(border: Border.all()),
+                child: Center(child: _localPreview()),
+              ),
+              const SizedBox(height: 10),
+              //Container for the Remote video
+              Container(
+                height: 240,
+                decoration: BoxDecoration(border: Border.all()),
+                child: Center(child: _remoteVideo()),
+              ),
+              // Button Row
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: _isJoined ? null : () => {join()},
+                      child: const Text("Join"),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: _isJoined ? () => {leave()} : null,
+                      child: const Text("Leave"),
+                    ),
+                  ),
+                ],
+              ),
+              // Button Row ends
+            ],
+          )),
+    );
+  }
+
+// Display local video preview
+  Widget _localPreview() {
+    if (_isJoined) {
+      return AgoraVideoView(
+        controller: VideoViewController(
+          rtcEngine: agoraEngine,
+          canvas: VideoCanvas(uid: 0),
+        ),
+      );
+    } else {
+      return const Text(
+        'Join a channel',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+// Display remote user's video
+  Widget _remoteVideo() {
+    if (_remoteUid != null) {
+      return AgoraVideoView(
+        controller: VideoViewController.remote(
+          rtcEngine: agoraEngine,
+          canvas: VideoCanvas(uid: _remoteUid),
+          connection: RtcConnection(channelId: channelName),
+        ),
+      );
+    } else {
+      String msg = '';
+      if (_isJoined) msg = 'Waiting for a remote user to join';
+      return Text(
+        msg,
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    // Set up an instance of Agora engine
+    setupVideoSDKEngine();
+  }
+
+
+  Future<void> setupVideoSDKEngine() async {
+    // retrieve or request camera and microphone permissions
+    await [Permission.microphone, Permission.camera].request();
+
+    //create an instance of the Agora engine
+    agoraEngine = createAgoraRtcEngine();
+    await agoraEngine.initialize(const RtcEngineContext(
+        appId: appId
+    ));
+
+    await agoraEngine.enableVideo();
+
+    // Register the event handler
+    agoraEngine.registerEventHandler(
+      RtcEngineEventHandler(
+        onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+          showMessage("Local user uid:${connection.localUid} joined the channel");
+          setState(() {
+            _isJoined = true;
+          });
+        },
+        onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+          showMessage("Remote user uid:$remoteUid joined the channel");
+          setState(() {
+            _remoteUid = remoteUid;
+          });
+        },
+        onUserOffline: (RtcConnection connection, int remoteUid,
+            UserOfflineReasonType reason) {
+          showMessage("Remote user uid:$remoteUid left the channel");
+          setState(() {
+            _remoteUid = null;
+          });
+        },
+      ),
+    );
+  }
+  void  join() async {
+    await agoraEngine.startPreview();
+
+    // Set channel options including the client role and channel profile
+    ChannelMediaOptions options = const ChannelMediaOptions(
+      clientRoleType: ClientRoleType.clientRoleBroadcaster,
+      channelProfile: ChannelProfileType.channelProfileCommunication,
+    );
+
+    await agoraEngine.joinChannel(
+      token: token,
+      channelId: channelName,
+      options: options,
+      uid: uid,
+    );
+  }
+  void leave() {
+    setState(() {
+      _isJoined = false;
+      _remoteUid = null;
+    });
+    agoraEngine.leaveChannel();
+  }
+// Release the resources when you leave
+  @override
+  void dispose() async {
+    await agoraEngine.leaveChannel();
+    agoraEngine.release();
+    super.dispose();
+  }
+
+  showMessage(String message) {
+    scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+}
+
